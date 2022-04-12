b357d545760829d2b1460b7469ea774403c515cd
Suparno-0069 <suparnochakraborty69@gmail.com>
2022-04-13 00:53:11 +0530

    unnecessary changes xD

--- php/login.php
+++ php/login.php
@@ -2,6 +2,7 @@
 session_start();
 include_once "dbconn.php";
 
+
 if ($_POST) {
 
     $un = $_POST['uname'];
@@ -10,8 +11,9 @@ if ($_POST) {
 
 
     $sql = "SELECT * FROM users WHERE uname='$un' AND passwords='$pass'";
+    $res = $conn->query($sql);
 
-    if ($conn->query($sql)) {
+    if ($res->num_rows > 0) {
         // echo "Login success!";
         $_SESSION["logged"] = true;
         header("Location: dashboard.php");
@@ -29,76 +31,3 @@ if ($_POST) {
 
     // }
 }
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
-// echo "<html>
-// <head>
-// </head>
-// <body>
-// <table>
-// <thead>
-//     <th scope='col'>User ID</th>
-//     <th scope='col'>Name</th>
-//     <th scope='col'>Email</th>
-//     <th scope='col'>Mobile</th>
-// </thead>
-// <tbody>";
-//     $uid = $row["userid"];
-//     $fname = $row["fname"];
-//     $lname = $row["lname"];
-//     $email = $row["email"];
-//     $mob = $row["mob"];
-
-//     echo "<tr>
-//             <td>$uid</td>
-//             <td>$fname$lname</td>
-//             <td>$email</td>
-//             <td>$mob</td>
-//         </tr>
-//     </tbody>
-// </table>
-// </body>
-// </html>";
-
-/*$result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $rl = $row["roll"];
-                $fname = $row["fname"];
-                $lname = $row["lname"];
-
-                echo "<tr>
-                        <th scope='row'>$rl</th>
-                        <td>$fname$lname</td>
-                        ";
-            }
-        } */
-?>
