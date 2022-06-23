9823d4ac441764d3182fec2a58bf7023f7ce2a63
Suparno-0069 <suparnochakraborty69@gmail.com>
2022-06-23 14:55:03 +0530

    clean up 2.3.1

--- php/requestBook.php
+++ php/requestBook.php
@@ -2,14 +2,14 @@
 session_start();
 include "dbconn.php";
 
-if($_SESSION["logged"]){
+if ($_SESSION["logged"]) {
 
     if ($_POST) {
         $un = $_SESSION["sun"];
         $sql = "SELECT * FROM users WHERE uname='$un'";
         $res = $conn->query($sql);
         $row = $res->fetch_assoc();
-        if($row){
+        if ($row) {
             $uid = $row["usrid"];
         }
         // $uid = ;
@@ -17,25 +17,26 @@ if($_SESSION["logged"]){
         $bname = $_POST["bname"];
         $pname = $_POST["pname"];
         $msg = $_POST["msg"];
-    
+
         // $pid = getIdStr(6);
-    
+
         $sql = "INSERT INTO requests(usrid, book_name, author_name, publisher_name, descriptions)
         VALUE('$uid', '$bname', '$aname', '$pname', '$msg')";
-    
-        if($conn->query($sql)){
+
+        if ($conn->query($sql)) {
             $_SESSION["requested"] = true;
-            header("Location: rqstSuccess.php");
+            echo "<script>alert('Request submitted! We will look into the matter');
+            window.location = 'dashboard.php'</script>";
             // echo "Data Inserted Successfully!";
-    
-        }else {
+
+        } else {
             echo "something went wrong" . $conn->error;
         }
-    // $sql = "INSERT INTO users(fname, lname, gender, dob, mob, email, addresses, country, uname, passwords)
-    // VALUE('$fn', '$ln', '$gen', '$dob', '$mob', '$em', '$addr', '$cn', '$un', '$pass')";
-    
-    
+        // $sql = "INSERT INTO users(fname, lname, gender, dob, mob, email, addresses, country, uname, passwords)
+        // VALUE('$fn', '$ln', '$gen', '$dob', '$mob', '$em', '$addr', '$cn', '$un', '$pass')";
+
+
     }
-}else {
+} else {
     header("Location: ../html/login.html");
 }
