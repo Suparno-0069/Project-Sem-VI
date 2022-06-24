9af4238df4de7e40abf7fbcb4aae66832ecbfa5d
Ankit-4920 <ankitshaw4920@gmail.com>
2022-06-24 11:50:13 +0530

    adding root folder

--- pdf-reader/eb_buyNow.php
+++ pdf-reader/eb_buyNow.php
@@ -44,7 +44,7 @@ if ($_SESSION["logged"]) {
                         <input type="submit" name="chkOutOnline" value="proceed to checkout" class="submit-btn" id="chkOutOnline" disabled>
                     </form>
                 </div>
-                <a id="backtocart" href="cart.php">Cancel payment and return to Cart</a>
+                <a id="backtocart" href="../php/cart.php">Cancel payment and return to Cart</a>
             </div>
 
             <script>
